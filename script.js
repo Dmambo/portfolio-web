d3da2bd709b833851659b45aecd6c1fb9a73839e
Dmambo <mambotrader0@gmail.com>
2022-11-15 19:16:09 +0530

    Modified my js file for linter errors

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const headers = document.querySelector('.headers');
 const list = document.querySelector('.list-items');
 
 bars.addEventListener('click', () => {
-  headers.classList.toggle('active')
+  headers.classList.toggle('active');
 });
 
 list.addEventListener('click', () => {
