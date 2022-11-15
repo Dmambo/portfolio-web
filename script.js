449c5395310e8129069c73166e688f8d3d0d49a3
Dmambo <mambotrader0@gmail.com>
2022-11-15 18:32:30 +0530

    Added script.js file for javascript functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+const bars = document.querySelector('.bars')
+const headers = document.querySelector('.headers')
+const list = document.querySelector('.list-items')
+
+bars.addEventListener('click', () => {
+    headers.classList.toggle("active")
+})
+
+list.addEventListener('click', () =>{
+    headers.classList.toggle("active")
+})
