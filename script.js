4d123136771c9db2239941169841a46d86d206f0
Dmambo <mambotrader0@gmail.com>
2022-11-15 19:07:03 +0530

    Modified my js for linter errors

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const bars = document.querySelector('.bars')
-const headers = document.querySelector('.headers')
-const list = document.querySelector('.list-items')
+const bars = document.querySelector('.bars');
+const headers = document.querySelector('.headers');
+const list = document.querySelector('.list-items');
 
 bars.addEventListener('click', () => {
-    headers.classList.toggle("active")
-})
+  headers.classList.toggle('active')
+});
 
-list.addEventListener('click', () =>{
-    headers.classList.toggle("active")
-})
+list.addEventListener('click', () => {
+  headers.classList.toggle('active');
+});
